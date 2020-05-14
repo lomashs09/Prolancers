886c8fee3f3f3cf5929bd9e3f32924c7c4118fe3
Praveen Sharma <praveensharma.inbox@gmail.com>
2020-05-14 09:08:36 +0530

    Added config based on environment

--- server.js
+++ server.js
@@ -14,6 +14,13 @@ mongoose.connect('mongodb+srv://dbCloud:$GET(PRO)@cluster0-bzeio.mongodb.net/Enq
 })
 const app = require('./app')
 
-app.listen(process.env.PORT,()=>{
-    console.log("Server running on PORT "+process.env.PORT+"!")
+let PORT = 3000;
+
+if(app.get('env')==='production'){
+    PORT = process.env.PORT
+}
+
+app.listen(PORT,()=>{
+    console.log(app.get('env'))
+    console.log("Server running on PORT "+PORT+"!")
 })
